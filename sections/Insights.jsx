87befc62cc954bc0d944ac3e1c1bfb0c20057d3c
STEVEN A BOWMAN <stevenabowman@STEVENs-MBP.lan>
2024-12-25 19:47:52 -0500

    Insight Page

--- sections/Insights.jsx
+++ sections/Insights.jsx
@@ -3,10 +3,9 @@
 import { motion } from 'framer-motion';
 
 import styles from '../styles';
+import { insights } from '../constants';
 import { staggerContainer } from '../utils/motion';
 import { InsightCard, TitleText, TypingText } from '../components';
-import { insights } from '../constants';
-
 
 const Insights = () => (
   <section className={`${styles.paddings} relative z-10`}>
@@ -17,17 +16,13 @@ const Insights = () => (
       viewport={{ once: false, amount: 0.25 }}
       className={`${styles.innerWidth} mx-auto flex flex-col`}
     >
-      <TypingText title="| Insight"
-      textStyles= "text-center" />
-      <TitleText title="Insight about Metaverse"
-      textStyles="text-center" />
-       <div className="mt-[50px] flex flex-col gap-[30px]">
-          {insights.map((insight, index) => (
-            <InsightCard key={`insight-${index}`} {...insight} index={ index + 1} />
-
-        
-          ))}
-       </div>
+      <TypingText title="| Insight" textStyles="text-center" />
+      <TitleText title={<>Insight about metaverse</>} textStyles="text-center" />
+      <div className="mt-[50px] flex flex-col gap-[30px]">
+        {insights.map((item, index) => (
+          <InsightCard key={`insight-${index}`} {...item} index={index + 1} />
+        ))}
+      </div>
     </motion.div>
   </section>
 );
