c1184b3d22ac00856dffee8e8b0917022e5a69fa
STEVEN A BOWMAN <stevenabowman@Mac.lan>
2025-01-05 20:48:52 -0500

    Out Folder Export

--- components/InsightCard.jsx
+++ components/InsightCard.jsx
@@ -24,7 +24,8 @@ const InsightCard = ({ imgUrl, title, subtitle, index }) => (
         </p>
       </div>
 
-      <div className="lg:flex hidden items-center justify-center w-[100px] h-[100px] rounded-full bg-transparent border-[1px] border-white"
+      <div
+        className="lg:flex hidden items-center justify-center w-[100px] h-[100px] rounded-full bg-transparent border-[1px] border-white"
       >
         <img
           src="/arrow.svg"
